e3421d088a796697be657c2a0f7ffc89e6284e88
bsharathramesh <bsharathramesh@gmail.com>
2020-10-24 12:48:50 -0700

    Modification to the Places Recommendation

--- Phase2_TripSage/Places_Recommendation/places_recommendation.py
+++ Phase2_TripSage/Places_Recommendation/places_recommendation.py
@@ -6,50 +6,50 @@ from datetime import datetime
 #from geopy.geocoders import Nominatim
 #import pandas as pd
 
+def Places_Recommendation(Gobj, Place, PlaceType):
+    Address = Place
+    geocode_result = gmaps.geocode(Address)
+    x = geocode_result[0]['geometry']['location']['lat']
+    y = geocode_result[0]['geometry']['location']['lng']
+    coordinate_string = str(x) + ',' + str(y)
+    
+    places_result  = gmaps.places_nearby(location= coordinate_string, radius = 40000, open_now =False , type = PlaceType)
+    time.sleep(3)
 
+    place_result  = gmaps.places_nearby(page_token = places_result['next_page_token'])
+    stored_results = []
 
+    # loop through each of the places in the results, and get the place details.      
+    for place in places_result['results']:
 
+        # define the place id, needed to get place details. Formatted as a string.
+        my_place_id = place['place_id']
 
-API_KEY = 'AIzaSyCOUCDt77J8v4d2BnWcarXbHzsJpIAhNVQ'
+        # define the fields you would liked return. Formatted as a list.
+        my_fields = ['name','formatted_phone_number','website','geometry/location','opening_hours', 'formatted_address']
 
+        # make a request for the details.
+        places_details  = gmaps.place(place_id= my_place_id , fields= my_fields)
 
+        # store the results in a list object.
+        stored_results.append(places_details['result'])
+    
+    return stored_results
 
 # Define the Client
+API_KEY = 'AIzaSyCOUCDt77J8v4d2BnWcarXbHzsJpIAhNVQ'
 gmaps = googlemaps.Client(key = API_KEY)
 
-# Do a simple nearby search where we specify the location
-# in lat/lon format, along with a radius measured in meters
+# Main Dictionary
+places_recommendation = {}
 
-Address = 'Chicago'
-geocode_result = gmaps.geocode(Address)
-x = geocode_result[0]['geometry']['location']['lat']
-y = geocode_result[0]['geometry']['location']['lng']
-coordinate_string = str(x) + ',' + str(y)
-
-places_result  = gmaps.places_nearby(location= coordinate_string, radius = 50000, open_now =False , type = 'tourist_attraction')
-time.sleep(3)
-
-place_result  = gmaps.places_nearby(page_token = places_result['next_page_token'])
-
-stored_results = []
-
-# loop through each of the places in the results, and get the place details.      
-for place in places_result['results']:
-
-    # define the place id, needed to get place details. Formatted as a string.
-    my_place_id = place['place_id']
-
-    # define the fields you would liked return. Formatted as a list.
-    my_fields = ['name','formatted_phone_number','website']
-
-    # make a request for the details.
-    places_details  = gmaps.place(place_id= my_place_id , fields= my_fields)
-
-    # print the results of the details, returned as a dictionary.
-    #pprint.pprint(places_details['result'])
-
-    # store the results in a list object.
-    stored_results.append(places_details['result'])
-
+# Fetching the tourist attraction near source and destination. 
+stored_results = Places_Recommendation(gmaps, 'Chicago', 'tourist_attraction')
 pprint.pprint(stored_results)
+
+
+
+
+
+
 
